a33a91892ca0bf35539b455b5851a0601de2e885
Kirill <k3499@yandex.ru>
2023-01-10 17:01:59 +0300

    content filled

--- src/components/NewcomerToolsList/NewcomerToolsList.js
+++ src/components/NewcomerToolsList/NewcomerToolsList.js
@@ -6,9 +6,34 @@ function NewcomerToolsList() {
       <div className="newcomerToolsList__item-block">
         <div className="newcomerToolsList__orange-circle"></div>
         <div className="newcomerToolsList__head-block">
-          <div className="newcomerToolsList__logo figma"></div>
+          <div className="newcomerToolsList__logo zeplin"></div>
           <div className="newcomerToolsList__title-block">
-            <h3 className="newcomerToolsList__title">FIGMA</h3>
+            <h3 className="newcomerToolsList__title">Zeplin</h3>
+            <p className="newcomerToolsList__pay-status">Free & Paid</p>
+          </div>
+        </div>
+        <p className="newcomerToolsList__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <div className="newcomerToolsList__use-bar">
+          <div className="newcomerToolsList__use-bar-left">
+            <a href="#" className="newcomerToolsList__like">
+              like
+            </a>
+            <a href="#" className="newcomerToolsList__add">
+              bookmark
+            </a>
+          </div>
+          <a href="#" className="newcomerToolsList__visit">
+            Visit
+          </a>
+        </div>
+      </div>
+      <div className="newcomerToolsList__item-block">
+        <div className="newcomerToolsList__head-block">
+          <div className="newcomerToolsList__logo phpstorm"></div>
+          <div className="newcomerToolsList__title-block">
+            <h3 className="newcomerToolsList__title">PHPStorm</h3>
             <p className="newcomerToolsList__pay-status">Free</p>
           </div>
         </div>
@@ -31,9 +56,9 @@ function NewcomerToolsList() {
       </div>
       <div className="newcomerToolsList__item-block">
         <div className="newcomerToolsList__head-block">
-          <div className="newcomerToolsList__logo figma"></div>
+          <div className="newcomerToolsList__logo toolbox"></div>
           <div className="newcomerToolsList__title-block">
-            <h3 className="newcomerToolsList__title">FIGMA</h3>
+            <h3 className="newcomerToolsList__title">Toolbox</h3>
             <p className="newcomerToolsList__pay-status">Free</p>
           </div>
         </div>
@@ -56,35 +81,10 @@ function NewcomerToolsList() {
       </div>
       <div className="newcomerToolsList__item-block">
         <div className="newcomerToolsList__head-block">
-          <div className="newcomerToolsList__logo figma"></div>
+          <div className="newcomerToolsList__logo procreate"></div>
           <div className="newcomerToolsList__title-block">
-            <h3 className="newcomerToolsList__title">FIGMA</h3>
-            <p className="newcomerToolsList__pay-status">Free</p>
-          </div>
-        </div>
-        <p className="newcomerToolsList__description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <div className="newcomerToolsList__use-bar">
-          <div className="newcomerToolsList__use-bar-left">
-            <a href="#" className="newcomerToolsList__like">
-              like
-            </a>
-            <a href="#" className="newcomerToolsList__add">
-              bookmark
-            </a>
-          </div>
-          <a href="#" className="newcomerToolsList__visit">
-            Visit
-          </a>
-        </div>
-      </div>
-      <div className="newcomerToolsList__item-block">
-        <div className="newcomerToolsList__head-block">
-          <div className="newcomerToolsList__logo figma"></div>
-          <div className="newcomerToolsList__title-block">
-            <h3 className="newcomerToolsList__title">FIGMA</h3>
-            <p className="newcomerToolsList__pay-status">Free</p>
+            <h3 className="newcomerToolsList__title">Procreate</h3>
+            <p className="newcomerToolsList__pay-status">Paid</p>
           </div>
         </div>
         <p className="newcomerToolsList__description">
--- src/components/PopularToolsList/PopularToolsList.js
+++ src/components/PopularToolsList/PopularToolsList.js
@@ -31,9 +31,34 @@ function PopularToolsList() {
       </div>
       <div className="popularToolsList__item-block">
         <div className="popularToolsList__head-block">
-          <div className="popularToolsList__logo figma"></div>
+          <div className="popularToolsList__logo sketch"></div>
           <div className="popularToolsList__title-block">
-            <h3 className="popularToolsList__title">FIGMA</h3>
+            <h3 className="popularToolsList__title">Sketch</h3>
+            <p className="popularToolsList__pay-status">Trial & Paid</p>
+          </div>
+        </div>
+        <p className="popularToolsList__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+        <div className="popularToolsList__use-bar">
+          <div className="popularToolsList__use-bar-left">
+            <a href="#" className="popularToolsList__like">
+              like
+            </a>
+            <a href="#" className="popularToolsList__add">
+              bookmark
+            </a>
+          </div>
+          <a href="#" className="popularToolsList__visit">
+            Visit
+          </a>
+        </div>
+      </div>
+      <div className="popularToolsList__item-block">
+        <div className="popularToolsList__head-block">
+          <div className="popularToolsList__logo vs"></div>
+          <div className="popularToolsList__title-block">
+            <h3 className="popularToolsList__title">Visual Studio Code</h3>
             <p className="popularToolsList__pay-status">Free</p>
           </div>
         </div>
@@ -56,10 +81,10 @@ function PopularToolsList() {
       </div>
       <div className="popularToolsList__item-block">
         <div className="popularToolsList__head-block">
-          <div className="popularToolsList__logo figma"></div>
+          <div className="popularToolsList__logo notion"></div>
           <div className="popularToolsList__title-block">
-            <h3 className="popularToolsList__title">FIGMA</h3>
-            <p className="popularToolsList__pay-status">Free</p>
+            <h3 className="popularToolsList__title">Notion</h3>
+            <p className="popularToolsList__pay-status">Free & Paid</p>
           </div>
         </div>
         <p className="popularToolsList__description">
@@ -81,10 +106,10 @@ function PopularToolsList() {
       </div>
       <div className="popularToolsList__item-block">
         <div className="popularToolsList__head-block">
-          <div className="popularToolsList__logo figma"></div>
+          <div className="popularToolsList__logo slack"></div>
           <div className="popularToolsList__title-block">
-            <h3 className="popularToolsList__title">FIGMA</h3>
-            <p className="popularToolsList__pay-status">Free</p>
+            <h3 className="popularToolsList__title">Slack</h3>
+            <p className="popularToolsList__pay-status">Free & Paid</p>
           </div>
         </div>
         <p className="popularToolsList__description">
@@ -106,35 +131,10 @@ function PopularToolsList() {
       </div>
       <div className="popularToolsList__item-block">
         <div className="popularToolsList__head-block">
-          <div className="popularToolsList__logo figma"></div>
+          <div className="popularToolsList__logo invision"></div>
           <div className="popularToolsList__title-block">
-            <h3 className="popularToolsList__title">FIGMA</h3>
-            <p className="popularToolsList__pay-status">Free</p>
-          </div>
-        </div>
-        <p className="popularToolsList__description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
-        <div className="popularToolsList__use-bar">
-          <div className="popularToolsList__use-bar-left">
-            <a href="#" className="popularToolsList__like">
-              like
-            </a>
-            <a href="#" className="popularToolsList__add">
-              bookmark
-            </a>
-          </div>
-          <a href="#" className="popularToolsList__visit">
-            Visit
-          </a>
-        </div>
-      </div>
-      <div className="popularToolsList__item-block">
-        <div className="popularToolsList__head-block">
-          <div className="popularToolsList__logo figma"></div>
-          <div className="popularToolsList__title-block">
-            <h3 className="popularToolsList__title">FIGMA</h3>
-            <p className="popularToolsList__pay-status">Free</p>
+            <h3 className="popularToolsList__title">Invision</h3>
+            <p className="popularToolsList__pay-status">Free & Paid</p>
           </div>
         </div>
         <p className="popularToolsList__description">
